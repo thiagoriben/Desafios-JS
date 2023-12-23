d50b982dbf8b5b285c2f88074b3d380b7d183751
Thiago Ribeiro Negreiros <thiagoriben@gmail.com>
2023-12-23 16:22:26 -0300

    v1

---
+++ desafio013/script.js
@@ -0,0 +1,13 @@
+let numerosRecebidos = []
+let posicaoDoArray = 0
+
+let quantidade = prompt('quantos números você deseja inserir?')
+
+for (let c1 = 0; c1 < quantidade; c1++ ){
+    var valorInserido = Number(prompt('preencha o array: ' + c1))
+    posicaoDoArray ++
+}
+
+numerosRecebidos [posicaoDoArray] = valorInserido
+
+console.log (numerosRecebidos)
