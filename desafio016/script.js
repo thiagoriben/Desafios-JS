24e8cde437c17d36af7766939a9a1061832a980c
Thiago Ribeiro Negreiros <thiagoriben@gmail.com>
2024-01-12 19:38:42 -0300

    pt1

---
+++ desafio016/script.js
@@ -0,0 +1,39 @@
+let arrayUsuario = []
+let arraySenha = []
+let repetir = true
+let index = 0
+
+while (repetir === true) {
+    let pergunta = prompt("O que você deseja? CADASTRAR = 1, FAZER LOGIN = 2, EXCLUIR UM CADASTRO = 3, OU ENCERRAR O PROGRAMA = 4.")
+
+    switch (pergunta) {
+        case "1":
+            alert("Você escolheu se cadastrar!")
+            let usuario = prompt("Insira o seu usuario.")   
+            arrayUsuario[index] = usuario 
+            let senha = prompt("Insira a sua senha.")    
+            arraySenha[index] = senha
+
+            index++
+            break;
+
+        case "2":
+            alert("Você escolheu fazer login!")
+            
+            break;
+        
+        
+        case "3":
+            alert("Você escolheu fazer login!")
+                
+            break;
+    
+
+        default:
+            alert("Você escolheu encerrar o programa.")
+            repetir = false
+            break;
+    }
+}
+
+console.log(arraySenha, arrayUsuario)
